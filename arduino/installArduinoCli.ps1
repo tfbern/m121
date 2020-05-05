125cd7ad632dadd742cc8ca59308f328f494a056
Sam <sam.hess@me.com>
2020-05-05 21:06:40 +0200

    update install script

--- arduino/installArduinoCli.ps1
+++ arduino/installArduinoCli.ps1
@@ -1,5 +1,31 @@
- $url = 'https://github.com/arduino/arduino-cli/releases/download/0.10.0/arduino-cli_0.10.0_Windows_64bit.zip'
- $tmp = 'arduino-cli.zip'
- Invoke-WebRequest $url -OutFile $tmp
- Expand-Archive -Path $tmp -DestinationPath  $($env:APPDATA + '\npm')
- $tmp | Remove-Itemget
+<#
+.DESCRIPTION
+A longer description of your script.
+
+.PARAMETER url
+URL of the current Arduino CLI Release
+
+.EXAMPLE
+PS> Install-ArduinoCli
+#>
+
+Param(
+  [string]$url = "https://github.com/arduino/arduino-cli/releases/download/0.10.0/arduino-cli_0.10.0_Windows_64bit.zip",
+  [string]$installPath = $($env:APPDATA + '\npm')
+)
+
+$file = $Env:TEMP + '\arduino-cli.zip'
+Invoke-WebRequest $url -OutFile $file
+if (Test-Path $file) {
+  Expand-Archive -Path $file -DestinationPath $installPath
+  $file | Remove-Itemget
+}
+
+try {
+  $(arduino-cli.exe version)
+  Write-Host "Installation successful"
+} catch {
+  Write-Host "Installation failed"
+}
+
+
